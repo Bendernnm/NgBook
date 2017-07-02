c3f50b67d4d876eee7e601a7e2f956ed8b7b5c7a
Bendernnm <bender.d1mon@gmail.com>
2017-07-02 19:02:17 +0300

    user-list and user-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
